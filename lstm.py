bc7ca4696159f16cff5e265cfc8a2ec548915f1c
evan <evanjulius66@yahoo.com>
2024-04-28 16:10:04 -0500

    updating lstm

--- lstm.py
+++ lstm.py
@@ -10,7 +10,6 @@ from tensorflow.keras.callbacks import EarlyStopping
 from argparse import ArgumentParser
 arg = ArgumentParser()
 
-from api import SP_Client
 
 user = os.getenv('SPACETRACKER_UNAME')
 password = os.getenv('SP_PASSWORD')
@@ -19,11 +18,6 @@ def clear():
     import os
     os.system('clear')
 
-sp = SP_Client(user, password)
-sp.set_data()
-
-line_data_1 = sp.tle_df_line1
-line_data_2 = sp.tle_df_line2
 
 arg.add_argument('--info', action='store_true', help='Display information about the file and dataset')
 arg.add_argument('--train', action='store_true', help='Run training function')
@@ -41,21 +35,20 @@ class K_LSTM:
         self.model.compile(loss='binary_crossentropy', optimizer='adam')
     
     def train(self, X_train, y_train):
-        early_stopping = EarlyStopping(monitor='val_loss', patience=10)
-
-        self.model.fit(X_train, y_train, epochs=200, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+        # early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+        self.model.fit(X_train, y_train, epochs=200, batch_size=32)
 
     def predict(self, X_test):
 
-        # X_test = X_test.reshape(X_test.shape[0], 1, -1)
-        # X_test = np.array(X_test).reshape(X_test.shape[0], 1, -1)
-        # prob_pred = self.model.predict(X_test)
-        # rounded_pred = np.round(prob_pred)
-        # # predictions = tf.cast(rounded_pred, tf.int32).numpy()
-        # # return predictions.flatten()
-        # return rounded_pred.flatten()
+        X_test = X_test.reshape(X_test.shape[0], 1, -1)
+        X_test = np.array(X_test).reshape(X_test.shape[0], 1, -1)
+        prob_pred = self.model.predict(X_test)
+        rounded_pred = np.round(prob_pred)
+        # predictions = tf.cast(rounded_pred, tf.int32).numpy()
+        # return predictions.flatten()
+        return rounded_pred.flatten()
 
-        return self.model.predict(X_test)
+        # return self.model.predict(X_test)
     
     def compile(self):
         self.model.compile(loss='binary_crossentropy', optimizer=RMSprop(learning_rate=0.001, rho=0.9))
@@ -72,8 +65,6 @@ class K_LSTM:
     def summary(self):
         print(self.model.summary())
         
-
-from sklearn.metrics import classification_report
 
 
 # Generate random data for X and y
@@ -93,33 +84,14 @@ X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
 if __name__ == "__main__":
     args = arg.parse_args()
-    lstm = K_LSTM(64, 16, 1)
-
-    y_pred = None
-
     if args.info:
         clear()
         print('This is a LSTM for the SpaceTracker API \n')
-        lstm.summary()
-        lstm.evaluate(X_test, y_test)
     elif args.train:
-        lstm.train(X_train, y_train)
-        y = lstm.predict(X_test)
-        
         print('Model trained successfully')
-        print('Model evaluation: ', lstm.evaluate(X_test, y_test))
+        print('Model evaluation: ')
     elif args.use_trustee:
+        print('Setting up trustee...')
+        pass
 
-        trustee = ClassificationTrustee(expert=lstm)
-        X_train_2d = X_train.reshape(X_train.shape[0],-1)
-        trustee.fit(X_train_2d, y_train, num_iter=50, num_stability_iter=10, samples_size=0.3, verbose=True)
-        dt, pruned_dt, agreement, reward = trustee.explain()
-        dt_y_pred = dt.predict(X_test.reshape(X_test.shape[0], -1))
-
-        print("Model explanation global fidelity report:")
-        print(classification_report(y_pred, dt_y_pred))
-        print("Model explanation score report:")
-        print(classification_report(y_test, dt_y_pred))
-
-    # elif args.attack_model:
 
